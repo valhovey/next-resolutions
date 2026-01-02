abf1cfebc281b3cf7e991791900bffced6bf5a0f
valhovey <khoveyc@gmail.com>
2026-01-01 23:17:31 -0700

    Supporting base path for export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/Resolutions",
+  assetPrefix: "/Resolutions/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
